a9468aeb8a56cb2a27fd2793e6c1c6df312c02dd
Mateusz Łukaszewicz <mateusz.lukaszewicz@microsoft.wsei.edu.pl>
2023-10-10 16:44:42 +0200

    prawie

---
+++ Lab2/Models/Birth.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab2.Models
+{
+    public class Birth
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public bool IsValid()
+        {
+            return (Name != null || Date < DateTime.Now);
+
+        }
+
+        public int BirthCalc()
+        {
+            return DateTime.Now.Year - Date.Year;
+        }
+    }
+
+}
