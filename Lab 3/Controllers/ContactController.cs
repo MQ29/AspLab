c95728d71a365a4625de9e7c2632bd0392bead19
Mateusz Łukaszewicz <mateusz.lukaszewicz@microsoft.wsei.edu.pl>
2023-10-17 16:37:18 +0200

    Lab 3

---
+++ Lab 3/Controllers/ContactController.cs
@@ -0,0 +1,70 @@
+﻿using Lab_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_3.Controllers
+{
+    public class ContactController : Controller
+    {
+        static readonly Dictionary<int, Contact> _contacts = new Dictionary<int, Contact>();
+        static int id = 1;
+        public IActionResult Index()
+        {
+            return View(_contacts);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Contact model)
+        {
+            if(ModelState.IsValid) 
+            {
+                model.Id = id++;
+                _contacts.Add(model.Id, model);
+                return RedirectToAction("Index");
+            }
+            return View(); //ponownie wyswitl form
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Contact model)
+        {
+            if(ModelState.IsValid) 
+            {
+                _contacts[model.Id] = model;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id) 
+        {
+            return View(_contacts[id]);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Contact model)
+        {
+            _contacts.Remove(model.Id);
+            return RedirectToAction("Index");
+        }
+
+        
+        public IActionResult Details(Contact model)
+        {
+            return View(_contacts);
+        }
+    }
+}
---
+++ Lab 3/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_3.Models
+{
+    public class Contact
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Musisz podac imie")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Zbyt długie imie, max 50 znaków")]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email{ get; set; }
+        public string? Phone { get; set; }
+        public DateTime? Birth { get; set; }
+    }
+}
